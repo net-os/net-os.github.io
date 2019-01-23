bd214ba47f1202d6f50cb6e499b4b3ce44f4aa55
ultimate-games <42284551+ultimate-games@users.noreply.github.com>
2019-01-23 12:53:10 -0800

    Update java.js

--- java.js
+++ java.js
@@ -8,10 +8,13 @@ function startTime() {
 }
   var m = today.getMinutes();
   var s = today.getSeconds();
+  var mo = today.getMonth() + 1;
+  var d = today.getDate();
+  var y = today.getYear() - 100;
   m = checkTime(m);
   s = checkTime(s);
   document.getElementById('clock').innerHTML =
- "<b>" + h + ":" + m + "</b>";
+ mo + "/" + d + "/" + y + "   " + "<b>" + h + ":" + m + "</b>";
   var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
