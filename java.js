b50d4b71811fb26abafa63f4eddece2c3dbd327b
Brian Baldner <42284551+brianbaldner@users.noreply.github.com>
2019-03-04 15:51:51 -0800

    Update java.js

--- java.js
+++ java.js
@@ -87,3 +87,8 @@ function openFullscreen() {
     elem.msRequestFullscreen();
   }
 }
+
+function wallpaperChange() {
+        var wall = prompt("Please enter wallpaper url.", "https://");
+        document.getElementById("thestyle").innerHTML = "body:before {   content: '';  display: block;position: fixed; left: 0; top: 0; width: 100%; height: 100%; z-index: -10; background: url(" + wall + ") no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }";
+      }
