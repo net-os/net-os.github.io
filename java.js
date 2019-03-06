940689079b9ff315bf16931789273fd621eb36eb
Brian Baldner <42284551+brianbaldner@users.noreply.github.com>
2019-03-06 06:52:29 -0800

    Update java.js

--- java.js
+++ java.js
@@ -91,6 +91,9 @@ if (localStorage.getItem("wallpaper") === null) {
 localStorage.setItem("wallpaper", "https://net-os.github.io/wallpaper.jpg");	
 }
 function loadwall()  {
+	if (localStorage.getItem("wallpaper") === null) {
+localStorage.setItem("wallpaper", "https://net-os.github.io/wallpaper.jpg");
+	}
 var wall = localStorage.getItem("wallpaper");
         document.getElementById("thestyle").innerHTML = "body:before {   content: '';  display: block;position: fixed; left: 0; top: 0; width: 100%; height: 100%; z-index: -10; background: url(" + wall + ") no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }";
 }
