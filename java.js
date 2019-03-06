bf8c971fa4b28d0dc25158ef8db7ecb427abff08
Brian Baldner <brianbaldner@users.noreply.github.com>
2019-03-05 21:38:47 -0800

    Update

--- java.js
+++ java.js
@@ -87,10 +87,10 @@ function openFullscreen() {
     elem.msRequestFullscreen();
   }
 }
-
-function loadwall()  {
 if (localStorage.getItem("wallpaper") === null) {
-localStorage.setItem("wallpaper", "https://net-os.github.io/wallpaper.jpg");
+localStorage.setItem("wallpaper", "https://net-os.github.io/wallpaper.jpg");	
+}
+function loadwall()  {
 var wall = localStorage.getItem("wallpaper");
         document.getElementById("thestyle").innerHTML = "body:before {   content: '';  display: block;position: fixed; left: 0; top: 0; width: 100%; height: 100%; z-index: -10; background: url(" + wall + ") no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }";
 }
@@ -143,5 +143,4 @@ function lightMode() {
     document.getElementById("css").href = "/style.css";
     document.getElementById("netos").src = "/net.svg";
 	 localStorage.setItem("mode", "dark");
-}
 }
