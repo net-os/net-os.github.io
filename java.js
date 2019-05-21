d94e3658ac2847faeeebb34db8d6888ebdb032a4
Brian Baldner <42284551+brianbaldner@users.noreply.github.com>
2019-05-21 14:42:07 -0700

    Update java.js

--- java.js
+++ java.js
@@ -92,7 +92,7 @@ if (localStorage.getItem("wallpaper") === null) {
 
 function loadwall() {
     if (localStorage.getItem("wallpaper") === null) {
-        localStorage.setItem("wallpaper", "https://net-os.github.io/wallpaper.jpg");
+        localStorage.setItem("wallpaper", "https://net-os.github.io/background.png.jpg");
     }
     var wall = localStorage.getItem("wallpaper");
     document.getElementById("thestyle").innerHTML = "body:before {   content: '';  display: block;position: fixed; left: 0; top: 0; width: 100%; height: 100%; z-index: -10; background: url(" + wall + ") no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }";
