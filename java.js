d1352d372fd70dad5649ad49c13aea04fb88ffac
Brian Baldner <42284551+brianbaldner@users.noreply.github.com>
2019-09-12 10:25:13 -0700

    Update java.js

--- java.js
+++ java.js
@@ -99,6 +99,16 @@ function loadwall() {
     document.getElementById("thestyle").innerHTML = "body:before {   content: '';  display: block;position: fixed; left: 0; top: 0; width: 100%; height: 100%; z-index: -10; background: url(" + wall + ") no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }";
     }
 
+function loadapps() {
+    if (localStorage.getItem("apps") === null) {
+        var yeet = document.getElementById("apps").innerHTML;
+        localStorage.setItem("apps", yeet);
+    }
+    
+    var dfgh = localStorage.getItem("apps");
+    document.getElementById("apps").innerHTML = dfgh;
+    }
+
 function wallpaperChange() {
     var wall = prompt("Please enter wallpaper url.", "https://");
     document.getElementById("thestyle").innerHTML = "body:before {   content: '';  display: block;position: fixed; left: 0; top: 0; width: 100%; height: 100%; z-index: -10; background: url(" + wall + ") no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; }";
