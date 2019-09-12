c26b923aaf2770ac0cbe90009cdd03eb3bbc3b49
Brian Baldner <42284551+brianbaldner@users.noreply.github.com>
2019-09-11 17:56:13 -0700

    Update java.js

--- java.js
+++ java.js
@@ -158,8 +158,7 @@ function appchange() {
     var url = prompt("Enter the link you want the app to go to.");
     var icon = prompt("Enter the html of the icon from fontawesome.com/icons. CAN NOT BE A PRO ICON!");
     var funct = "openApp('" + url + "')"
-    document.getElementById(numb).onclick = funct
-    document.getElementById(numb).innerHTML = icon;
+    document.getElementById(numb).innerHTML = "<a onclick='" + funct + ">" + icon + "</a>" ;
     var approw = document.getElementById(numb).innerHTML;
     localStorage.setItem("approw", approw);
 }
