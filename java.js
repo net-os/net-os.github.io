c8a6c37b564ebb514fd25e621e3d0252c29a4bbf
Brian Baldner <42284551+brianbaldner@users.noreply.github.com>
2019-09-14 20:45:57 -0700

    Update java.js

--- java.js
+++ java.js
@@ -163,12 +163,7 @@ function reset() {
     location.reload();
 }
 function appchange() {
-    var numb = await Swal.fire({
-  title: 'Enter the number of the app you want to change. From left to right, 1-5.',
-  input: 'text',
-  inputValue: inputValue,
-  showCancelButton: true,
-})
+    var numb = prompt("Enter the number of the app you want to  change. From left to right, 1-5.");
     var url = prompt("Enter the link you want the app to go to.");
     var icon = prompt("Enter the html of the icon from fontawesome.com/icons or you can use a capitol letter. CAN NOT BE A PRO ICON!");
     var funct = "openApp('" + url + "')"
